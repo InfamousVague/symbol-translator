78434562ef84f2e47ad5aae54c6b409a4430d4e3
Matt Wisniewski <wski@Matts-Mac.local>
2017-07-21 19:27:47 -0600

    inital push

---
+++ build.js
@@ -0,0 +1,40 @@
+const supportedCurrencies = require('./supportedCurrencies.json')
+
+const walk = require('walk')
+const fs = require('fs')
+const walker = walk.walk('./dictionaries')
+
+let glossary = Object.assign({}, supportedCurrencies)
+
+walker.on('file', (root, fileStats, next) => {
+  const dictionary = require(`./dictionaries/${fileStats.name}`)
+  const tempGlossary = Object.assign({}, glossary)
+
+  Object.keys(dictionary).map((coin, i) => {
+    tempGlossary[coin] = Object.assign(
+      {}, 
+      tempGlossary[coin], 
+      {
+        service: Object.assign(
+          {},
+          glossary[coin].service,
+          {
+            [fileStats.name.replace('.json', '')]: dictionary[coin]
+          }
+        )
+      }
+    )
+    
+
+
+    if (i === Object.keys(dictionary).length - 1) {
+      glossary = Object.assign({}, glossary, tempGlossary)
+      next()
+    }
+  })
+})
+
+walker.on("end", function () {
+  console.log(glossary)
+  fs.writeFile('glossary.json', JSON.stringify(glossary, null, 2))
+})
